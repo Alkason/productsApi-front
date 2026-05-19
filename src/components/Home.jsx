3ebce1d09c6fd29b836b8574f71f4c5a5d8d777a
Alkason <116364036+Alkason@users.noreply.github.com>
2026-05-19 16:00:31 +0200

    Update API URL for product fetching

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   const [items, setItems] = useState([]);
   const [newProductName, setNewProductName] = useState('');
   const [editingProduct, setEditingProduct] = useState(null);
-  const apiUrl = 'https://products-api-dotnet-git-aleksanderczaj-dev.apps.rm1.0a51.p1.openshiftapps.com/api/products';
+  const apiUrl = 'https://products-api-backend-aleksanderczaj-dev.apps.rm1.0a51.p1.openshiftapps.com/api/products';
 
   const getProducts = () => {
     fetch(apiUrl)
